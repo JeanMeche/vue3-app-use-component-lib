33ed2d43615be8e2e98a337f49e4c49a907d150b
Matthieu Riegler <kyro38@gmail.com>
2021-04-01 00:17:46 +0200

    use component lib

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+require('vue-component-lib-with-storybook/dist/dummylib.css')
 
 createApp(App).use(router).mount('#app')
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,21 @@
 <template>
+  <MyHeader/>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
   </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component'
 import HelloWorld from '@/components/HelloWorld.vue' // @ is an alias to /src
+import { MyButton, MyHeader } from 'vue-component-lib-with-storybook'
 
 @Options({
   components: {
-    HelloWorld
+    HelloWorld,
+    MyButton,
+    MyHeader
   }
 })
 export default class Home extends Vue {}
